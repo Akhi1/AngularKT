9c7d18232fa04b063ca161d34bf2720c1cdbb8ea
Akhi1 <Akhi1@users.noreply.github.com>
2020-06-01 14:18:43 +0530

    complete application with studentdata introduced ngStyle and ngClass

---
+++ 2020-06-01/DemoApp/src/app/sections/sections.component.ts
@@ -0,0 +1,255 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-sections',
+  templateUrl: './sections.component.html',
+  styleUrls: ['./sections.component.css']
+})
+export class SectionsComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(response => {
+      //console.log(response);
+      this.department = response.dept;
+      this.checkSection();
+    });
+  }
+
+  sectionA=false; sectionB=false; sectionC=false; sectionD=false;
+  checkSection(){
+    for(let item of this.studentsdata){
+      if(item.department==this.department && item.section=='A'){
+        this.sectionA = true;
+      }
+      if(item.department==this.department && item.section=='B'){
+        this.sectionB = true;
+      }
+      if(item.department==this.department && item.section=='C'){
+        this.sectionC = true;
+      }
+      if(item.department==this.department && item.section=='D'){
+        this.sectionD = true;
+      }
+
+    }
+  }
+
+
+
+  department;
+
+  studentsdata = [
+    {
+      "department":"cse",
+      "section":"A",
+      "studentid":"cs101",
+      "studentname":"george",
+      "personalinfo":[
+        {
+          "place":"delhi",
+          "food":"idli",
+          "color":"green"
+        }
+      ]
+    },
+    {
+      "department":"mech",
+      "section":"A",
+      "studentid":"mech101",
+      "studentname":"martin",
+      "personalinfo":[
+        {
+          "place":"mumbai",
+          "food":"puri",
+          "color":"green"
+        }
+      ]
+    },
+    {
+      "department":"ece",
+      "section":"A",
+      "studentid":"ece101",
+      "studentname":"tim",
+      "personalinfo":[
+        {
+          "place":"hyderabad",
+          "food":"vada",
+          "color":"yellow"
+        }
+      ]
+    },
+    {
+      "department":"it",
+      "section":"A",
+      "studentid":"it101",
+      "studentname":"ronald",
+      "personalinfo":[
+        {
+          "place":"kurnool",
+          "food":"dosa",
+          "color":"red"
+        }
+      ]
+    },
+    {
+      "department":"cse",
+      "section":"B",
+      "studentid":"cs102",
+      "studentname":"robin",
+      "personalinfo":[
+        {
+          "place":"delhi",
+          "food":"idli",
+          "color":"green"
+        }
+      ]
+    },
+    {
+      "department":"ece",
+      "section":"B",
+      "studentid":"ece101",
+      "studentname":"shane",
+      "personalinfo":[
+        {
+          "place":"delhi",
+          "food":"idli",
+          "color":"green"
+        }
+      ]
+    },
+    {
+      "department":"mech",
+      "section":"B",
+      "studentid":"mech101",
+      "studentname":"jamal",
+      "personalinfo":[
+        {
+          "place":"delhi",
+          "food":"idli",
+          "color":"green"
+        }
+      ]
+    },
+    {
+      "department":"it",
+      "section":"B",
+      "studentid":"it101",
+      "studentname":"kim",
+      "personalinfo":[
+        {
+          "place":"delhi",
+          "food":"idli",
+          "color":"green"
+        }
+      ]
+    },
+    {
+      "department":"cse",
+      "section":"C",
+      "studentid":"cs103",
+      "studentname":"Drake",
+      "personalinfo":[
+        {
+          "place":"delhi",
+          "food":"idli",
+          "color":"green"
+        }
+      ]
+    },
+    {
+      "department":"ece",
+      "section":"C",
+      "studentid":"ece103",
+      "studentname":"carl",
+      "personalinfo":[
+        {
+          "place":"delhi",
+          "food":"idli",
+          "color":"green"
+        }
+      ]
+    },
+    {
+      "department":"mech",
+      "section":"C",
+      "studentid":"mech103",
+      "studentname":"travis",
+      "personalinfo":[
+        {
+          "place":"delhi",
+          "food":"idli",
+          "color":"green"
+        }
+      ]
+    },
+    {
+      "department":"it",
+      "section":"B",
+      "studentid":"it103",
+      "studentname":"sophie",
+      "personalinfo":[
+        {
+          "place":"delhi",
+          "food":"idli",
+          "color":"green"
+        }
+      ]
+    },
+{
+      "department":"cse",
+      "section":"D",
+      "studentid":"cs104",
+      "studentname":"brad",
+      "personalinfo":[
+        {
+          "place":"delhi",
+          "food":"idli",
+          "color":"green"
+        }
+      ]
+    },
+    {
+      "department":"ece",
+      "section":"D",
+      "studentid":"ece104",
+      "studentname":"robert",
+      "personalinfo":[
+        {
+          "place":"delhi",
+          "food":"idli",
+          "color":"green"
+        }
+      ]
+    },
+    {
+      "department":"mech",
+      "section":"D",
+      "studentid":"mech104",
+      "studentname":"stella",
+      "personalinfo":[
+        {
+          "place":"delhi",
+          "food":"idli",
+          "color":"green"
+        }
+      ]
+    },
+    {
+      "department":"it",
+      "section":"D",
+      "studentid":"it104",
+      "studentname":"mike",
+      "personalinfo":[
+        {
+          "place":"delhi",
+          "food":"idli",
+          "color":"green"
+        }
+      ]
+    }
+  ]
+
+}
